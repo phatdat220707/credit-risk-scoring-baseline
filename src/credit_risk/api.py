f695687c88bee8d49088875b5bfab490e45ae341
Dat <datnguyen220707@gmail.com>
2026-08-12 23:39:41 +0800

    Add API input validation

--- src/credit_risk/api.py
+++ src/credit_risk/api.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 MODEL_PATH = "models/logistic_regression_pipeline.joblib"
@@ -14,23 +14,23 @@ model = joblib.load(MODEL_PATH)
 
 class CreditRiskInput(BaseModel):
     checking_account_status: str
-    duration_months: int
+    duration_months: int = Field(gt=0)
     credit_history: str
     purpose: str
-    credit_amount: int
+    credit_amount: int = Field(gt=0)
     savings_account: str
     employment_since: str
-    installment_rate: int
+    installment_rate: int = Field(ge=1,le=4)
     personal_status_sex: str
     other_debtors: str
-    residence_since: int
+    residence_since: int = Field(ge=1,le=4)
     property: str
-    age: int
+    age: int = Field(gt=0)
     other_installment_plans: str
     housing: str
-    existing_credits: int
+    existing_credits: int = Field(ge=1)
     job: str
-    dependents: int
+    dependents: int = Field(ge=1)
     telephone: str
     foreign_worker: str
 
